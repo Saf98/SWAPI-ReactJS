d9c20a0ed48d0ec4ff94e90075498c82d6546f1c
Saf98 <sbilimoria98@gmail.com>
2022-02-01 23:14:39 +0000

    added some styling and refactored api structure

--- src/App.js
+++ src/App.js
@@ -1,68 +1,89 @@
 import './App.css';
-import { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import { getData } from './services/getData';
 
 function App() {
-  const [data, setData] = useState([]);
-  const [value, setValue] = useState('');
-  const [arr, setNewArr] = useState([]);
-  const [isFound, setIsFound] = useState(null);
-
+  const [charactersData, setCharactersData] = useState();
+  const [characterName, setCharacterName] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  
   useEffect(() => {
-    const getAPI = async () => {
-      const url = 'https://swapi.dev/api/people/';
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      });
-
-      const getData = await response.json();
-      setData(getData.results);
-    };
-
-    getAPI();
+    const apiData = getData();
+    apiData.then(data => {
+      console.log(data.results);
+      setCharactersData(data.results);
+    })
   }, []);
+  
 
-  const printThis = () => {
-    setNewArr(
-      data.filter((character) => 
-      character.name.toLowerCase().includes(value))
+  const checkCharacterName = () => {
+    setFilteredData(
+      charactersData.filter((character) => {
+        if (character.name) {
+          return character.name.toLowerCase().includes(characterName);
+        }
+
+        if (!character.name) {
+          console.log('There is not name available!');
+          return;
+        }
+      })
     )
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    printThis();
+    checkCharacterName();
   };
 
   return (
     <Fragment>
-      <form onSubmit={handleSubmit}>
-        <label>Search:</label>
-        <input
-          type='text'
-          id='searchBar'
-          value={value}
-          onChange={(e) => setValue(e.target.value.toLowerCase())}
-        >
-        </input>
-        <button type='submit'>
-          submit
-        </button>
-      </form>
-      {arr.length === 0 ? <p>no</p> : arr.map((element, index) => {
-        return (
-          <ul key={index}>
-            <li>{element.name}</li>
-            <li>{element.gender}</li>
-            <li>{element.birth_year}</li>
-          </ul>
-        );
-      })}
+      <div className='container'>
+        <div className='row'>
+          <div className='col search-container'>
+            <form onSubmit={handleSubmit}>
+              <label>Enter the character's name</label>
+              <input
+                type='text'
+                id='searchBar'
+                value={characterName}
+                onChange={(e) => setCharacterName(e.target.value.toLowerCase())}
+                
+              >
+              </input>
+              <button type='submit'>
+                Go!
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='row'>
+          {filteredData.map((element, index) => {
+            return (
+              <div className='col col-6'>
+                <div className='card-content'>
+                  <div className='card-item'>
+                    <p className='card-item_label'>Name</p>
+                    <p className='card-item_result'>{element.name}</p>
+                  </div>
+                  <div className='card-item'>
+                    <p className='card-item_label'>Birth year</p>
+                    <p className='card-item_result'>{element.birth_year}</p>
+                  </div>
+                  <div className='card-item'>
+                    <p className='card-item_label'>Gender</p>
+                    <p className='card-item_result'>{element.gender}</p>
+                  </div>
+                </div>
+            </div>
+            );
+          })}
+        </div>
+      </div>
     </Fragment>
   );
-
 }
 
 export default App;
