6de5fc3e8dccc0783d3745fb1afefff4cf1f831e
Saf98 <sbilimoria98@gmail.com>
2022-02-04 22:07:46 +0000

    refactor into components.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import './App.css';
 import React, { Fragment, useState, useEffect } from 'react';
 import { getData } from './services/getData';
+import Results from './components/Results';
+import Search from './components/Search';
 
 function App() {
-  const [charactersData, setCharactersData] = useState();
+  const [charactersData, setCharactersData] = useState(null);
   const [characterName, setCharacterName] = useState('');
-  const [filteredData, setFilteredData] = useState([]);
+  const [filteredData, setFilteredData] = useState(null);
   
   useEffect(() => {
     const apiData = getData();
     apiData.then(data => {
-      console.log(data.results);
       setCharactersData(data.results);
     })
   }, []);
   
-
   const checkCharacterName = () => {
+    console.log('check', charactersData);
     setFilteredData(
       charactersData.filter((character) => {
-        if (character.name) {
-          return character.name.toLowerCase().includes(characterName);
+        if (!character.name) {
+          console.warn('This property does not exist');
+          return null;
         }
 
-        if (!character.name) {
-          console.log('There is not name available!');
-          return;
-        }
+        return character.name.toLowerCase().includes(characterName);
       })
     )
   };
@@ -36,52 +35,16 @@ function App() {
     checkCharacterName();
   };
 
+  console.log(filteredData);
+
   return (
     <Fragment>
+      <Search handleSubmit={handleSubmit} character={characterName} onChange={(characterName) => setCharacterName(characterName)} />
       <div className='container'>
-        <div className='row'>
-          <div className='col search-container'>
-            <form onSubmit={handleSubmit}>
-              <label>Enter the character's name</label>
-              <input
-                type='text'
-                id='searchBar'
-                value={characterName}
-                onChange={(e) => setCharacterName(e.target.value.toLowerCase())}
-                
-              >
-              </input>
-              <button type='submit'>
-                Go!
-              </button>
-            </form>
-          </div>
+        <div className='row'>  
+          {filteredData && <Results data={filteredData} /> }
         </div>
-      </div>
-      <div className='container'>
-        <div className='row'>
-          {filteredData.map((element, index) => {
-            return (
-              <div className='col col-6'>
-                <div className='card-content'>
-                  <div className='card-item'>
-                    <p className='card-item_label'>Name</p>
-                    <p className='card-item_result'>{element.name}</p>
-                  </div>
-                  <div className='card-item'>
-                    <p className='card-item_label'>Birth year</p>
-                    <p className='card-item_result'>{element.birth_year}</p>
-                  </div>
-                  <div className='card-item'>
-                    <p className='card-item_label'>Gender</p>
-                    <p className='card-item_result'>{element.gender}</p>
-                  </div>
-                </div>
-            </div>
-            );
-          })}
-        </div>
-      </div>
+      </div>    
     </Fragment>
   );
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Results = ({ data }) => {
+  console.log('props:', data.length);
+  if (data.length > 0) {
+    return (
+      data.map((element, index) => {
+        return (
+          <div className='wrap' key={index}>
+            <div className='card-content'>
+              <div className='card-item'>
+                <p className='card-item_label'>Name</p>
+                <p className='card-item_result'>{element.name}</p>
+              </div>
+              <div className='card-item'>
+                <p className='card-item_label'>Birth year</p>
+                <p className='card-item_result'>{element.birth_year}</p>
+              </div>
+              <div className='card-item'>
+                <p className='card-item_label'>Gender</p>
+                <p className='card-item_result'>{element.gender}</p>
+              </div>
+            </div>
+          </div>
+        )
+      })
+    )
+  }
+
+  if (data.length === 0) {
+    return (
+      <div className='wrap'>
+        <div className='card-content'>
+          <div className='card-item'>
+            <p className='card-item_label'>no results</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
+
+export default Results
