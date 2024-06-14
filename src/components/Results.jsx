5f62cc7e8bf8dda8821b78723dc676fa0c315528
Saf98 <sbilimoria98@gmail.com>
2024-06-14 17:52:16 +0100

    remove cards and add tables

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,34 +2,21 @@ import React from "react";
 
 const Results = ({ data }) => {
   if (data.length > 0) {
-    return (
-      data.map((element, index) => {
-        return (
-          <div className='wrap' key={index}>
-            <div className='card-content'>
-              <div className='card-item'>
-                <p className='card-item_label'>Name</p>
-                <p className='card-item_result'>{element.name}</p>
-              </div>
-              <div className='card-item'>
-                <p className='card-item_label'>Birth year</p>
-                <p className='card-item_result'>{element.birth_year}</p>
-              </div>
-              <div className='card-item'>
-                <p className='card-item_label'>Gender</p>
-                <p className='card-item_result'>{element.gender}</p>
-              </div>
-            </div>
-          </div>
-        );
-      })
-    )
+    return data.map((element, index) => {
+      return (
+        <tr key={index}>
+          <td>{element.name}</td>
+          <td>{element.birth_year}</td>
+          <td>{element.gender}</td>
+        </tr>
+      );
+    });
   }
 
   if (data.length === 0) {
     return (
-      <div className='wrap'>
-        <div className='no-results'>
+      <div className="wrap">
+        <div className="no-results">
           <p>no results found</p>
         </div>
       </div>
@@ -37,4 +24,4 @@ const Results = ({ data }) => {
   }
 };
 
-export default Results
+export default Results;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,28 +1,39 @@
 import React from "react";
 
-const Search = ({handleSubmit, character, onChange}) => {
+const Search = ({ handleSubmit, character, onChange }) => {
   return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col search-container'>
+    <div className="container">
+      <div className="row">
+        <div className="col search-container">
           <form onSubmit={handleSubmit}>
             <label>Enter the character's name</label>
             <input
-              type='text'
-              id='searchBar'
+              type="text"
+              id="searchBar"
               value={character}
               onChange={(e) => onChange(e.target.value.toLowerCase())}
               required
-            >
-            </input>
-            <button type='submit'>
-              Go!
-            </button>
+            ></input>
+            <button type="submit">Go!</button>
           </form>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>DoB</th>
+                <th>Gender</th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Search;
