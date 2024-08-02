efb8e25305968f06094120ac6ad78253049e4f39
Saf98 <sbilimoria98@gmail.com>
2024-08-02 21:24:07 +0100

    Updates

--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-const Header = () => {
-  return <header>
-    <div className={"py-10 bg-neutral-900	"}>
-      <h1 className={"text-center text-amber-500 text-4xl font-bold"}>SWAPI</h1>
-    </div>
-  </header>
-}
-export default Header;
