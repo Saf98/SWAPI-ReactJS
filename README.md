d01683c5d19881cacb9f484c3258bb708e7c2aa9
Saf98 <sbilimoria98@gmail.com>
2024-08-09 14:47:53 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# StarWars API
-The goal of this project is to learn how to fetch data from an API and display the results.
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import logo from "../images/logo.png";
-const Header = () => {
-  return <header>
-    <div className={"header"}>
-      <img srcSet={logo} />
-    </div>
-  </header>
-}
-export default Header;
