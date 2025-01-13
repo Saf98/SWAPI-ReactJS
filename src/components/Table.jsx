f4d34097a127603878fc0cb2ae7d171e45d478fc
Saf98 <sbilimoria98@gmail.com>
2025-01-13 22:14:26 +0000

    API Change Fix

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,141 +1,141 @@
 import React, { Fragment, useMemo } from "react";
 
 const Table = ({
-  defaultFilterKeys,
-  visibleColumns,
-  sortColumn,
-  getFilteredRow,
-  results,
-  inputFieldValue,
-  sortingDirections,
+	defaultFilterKeys,
+	visibleColumns,
+	sortColumn,
+	getFilteredRow,
+	results,
+	inputFieldValue,
+	sortingDirections,
 }) => {
-  const filteredRows = useMemo(() => {
-    return getFilteredRow(results, inputFieldValue);
-  }, [getFilteredRow, results, inputFieldValue]);
+	const filteredRows = useMemo(() => {
+		return getFilteredRow(results, inputFieldValue);
+	}, [getFilteredRow, results, inputFieldValue]);
 
-  const getClassName = (value) => {
-    switch (value) {
-      case "brown":
-        return "brown";
-      case "blue":
-        return "blue";
-      case "blond":
-        return "blond";
-      case "blonde":
-        return "blond";
-      case "black":
-        return "black";
-      case "white":
-        return "white";
-      case "brown, grey":
-        return "grey";
-      case "blue-gray":
-        return "blue-gray";
-      case "auburn, white":
-        return "auburn";
-      case "auburn, grey":
-        return "grey";
-      case "grey":
-        return "grey";
-      case "yellow":
-        return "yellow";
-      case "red":
-        return "red";
-      case "orange":
-        return "orange";
-      case "hazel":
-        return "hazel";
-      case "pink":
-        return "pink";
-      case "red, blue":
-        return "red-blue";
-      case "green, yellow":
-        return "green-yellow";
-      case "gold":
-        return "gold";
-      default:
-        return "";
-    }
-  };
+	const getClassName = (value) => {
+		switch (value) {
+			case "brown":
+				return "brown";
+			case "blue":
+				return "blue";
+			case "blond":
+				return "blond";
+			case "blonde":
+				return "blond";
+			case "black":
+				return "black";
+			case "white":
+				return "white";
+			case "brown, grey":
+				return "grey";
+			case "blue-gray":
+				return "blue-gray";
+			case "auburn, white":
+				return "auburn";
+			case "auburn, grey":
+				return "grey";
+			case "grey":
+				return "grey";
+			case "yellow":
+				return "yellow";
+			case "red":
+				return "red";
+			case "orange":
+				return "orange";
+			case "hazel":
+				return "hazel";
+			case "pink":
+				return "pink";
+			case "red, blue":
+				return "red-blue";
+			case "green, yellow":
+				return "green-yellow";
+			case "gold":
+				return "gold";
+			default:
+				return "";
+		}
+	};
 
-  const tableRows = useMemo(() => {
-    return filteredRows?.map((row) => (
-      <tr key={row.id}>
-        {defaultFilterKeys.map((column) => {
-          const className = getClassName(row[column.id]);
-          return (
-            visibleColumns[column.id] && (
-              <td key={column.id}>
-                <span className={`pill ${className}`}>{row[column.id]}</span>
-              </td>
-            )
-          );
-        })}
-      </tr>
-    ));
-  }, [filteredRows, defaultFilterKeys, visibleColumns]);
+	const tableRows = useMemo(() => {
+		return filteredRows?.map((row) => (
+			<tr key={row.id}>
+				{defaultFilterKeys.map((column) => {
+					const className = getClassName(row[column.id]);
+					return (
+						visibleColumns[column.id] && (
+							<td key={column.id}>
+								<span className={`pill ${className}`}>{row[column.id]}</span>
+							</td>
+						)
+					);
+				})}
+			</tr>
+		));
+	}, [filteredRows, defaultFilterKeys, visibleColumns]);
 
-  const getClassNamesFor = (sortKey, sortingDirections) => {
-    switch (sortingDirections[sortKey]) {
-      case "UNSORTED":
-        return "g";
-      case "ASC":
-        return "a";
-      case "DESC":
-        return "s";
-      default:
-        return "g";
-    }
-  };
+	const getClassNamesFor = (sortKey, sortingDirections) => {
+		switch (sortingDirections[sortKey]) {
+			case "UNSORTED":
+				return "g";
+			case "ASC":
+				return "a";
+			case "DESC":
+				return "s";
+			default:
+				return "g";
+		}
+	};
 
-  return (
-    <Fragment>
-      <table className="results-table">
-        <thead align="left">
-          <tr>
-            {defaultFilterKeys.map(
-              (column) =>
-                visibleColumns[column.id] && (
-                  <>
-                    <th key={column.id}>
-                      <div
-                        onClick={() => sortColumn(column.id)}
-                        style={{
-                          display: "flex",
-                          flexWrap: "wrap",
-                          flexDirection: "row",
-                          justifyContent: "space-between",
-                        }}
-                      >
-                        <span>{column.label}</span>
-                        <button
-                          key={column.id}
-                          style={{
-                            fontFamily: "AttackPosition-Symbols",
-                            fontWeight: "normal",
-                            fontStyle: "normal",
-                            fontSize: "14px",
-                            backgroundColor: "transparent",
-                            border: 0,
-                            padding: 0,
-                            color: "white",
-                            display: "inline-block",
-                            marginLeft: "10px",
-                          }}
-                        >
-                          {getClassNamesFor(column.id, sortingDirections)}
-                        </button>
-                      </div>
-                    </th>
-                  </>
-                )
-            )}
-          </tr>
-        </thead>
-        <tbody>{tableRows}</tbody>
-      </table>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<table className="results-table">
+				<thead align="left">
+					<tr>
+						{defaultFilterKeys.map(
+							(column) =>
+								visibleColumns[column.id] && (
+									<>
+										<th key={column.id}>
+											<div
+												onClick={() => sortColumn(column.id)}
+												style={{
+													display: "flex",
+													flexWrap: "wrap",
+													flexDirection: "row",
+													justifyContent: "space-between",
+												}}
+											>
+												<span>{column.label}</span>
+												<button
+													key={column.id}
+													style={{
+														fontFamily: "AttackPosition-Symbols",
+														fontWeight: "normal",
+														fontStyle: "normal",
+														fontSize: "14px",
+														backgroundColor: "transparent",
+														border: 0,
+														padding: 0,
+														color: "white",
+														display: "inline-block",
+														marginLeft: "10px",
+													}}
+												>
+													{getClassNamesFor(column.id, sortingDirections)}
+												</button>
+											</div>
+										</th>
+									</>
+								)
+						)}
+					</tr>
+				</thead>
+				<tbody>{tableRows}</tbody>
+			</table>
+		</Fragment>
+	);
 };
 
 export default Table;
--- src/services/getData.js
+++ src/services/getData.js
@@ -1,12 +1,12 @@
 export const getData = async (currentPage) => {
-  const url = `https://swapi.dev/api/people/?page=${currentPage}`;
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+	const url = `https://swapi.tech/api/people/?page=${currentPage}`;
+	const response = await fetch(url, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
 
-  const data = await response.json();
-  return data;
+	const data = await response.json();
+	return data;
 };
